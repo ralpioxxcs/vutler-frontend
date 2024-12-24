94eede10e35e4277f4635d260c54d5caa81b56f3
YG1ee <fanfolderkim@gmail.com>
2024-12-24 18:43:47 +0900

    (main)/page와 event/page에 tanstack 적용

--- app/(main)/event/page.tsx
+++ app/(main)/event/page.tsx
@@ -3,79 +3,58 @@
 import CreateButton from "@/components/create-schedule";
 import ScheduleCard from "@/components/schedule-card";
 import { getScheduleList } from "@/pages/api/schedule";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 export default function EventPage() {
-  const [data, setData] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string>("");
+  const queryId = "event";
+  const { data, isLoading, isError } = useQuery({
+    queryKey: [queryId],
+    queryFn: () => getScheduleList("one_time", "event"),
+  });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-
-        const scheduleList = await getScheduleList("one_time", "event");
-        if (!scheduleList) {
-          throw new Error("failed to fetch data");
-        }
-        setData(scheduleList);
-      } catch (error) {
-        if(error instanceof Error) {
-          setError(error.message);
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  if (loading) {
+  if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
-  if (error) {
+  if (isError) {
     return <h1>Error</h1>;
   }
 
-return (
-    <div>
-      <div className="p-4">
-        <h1 className="text-3xl font-bold mb-4">My events</h1>
-        {data.length > 0 ? (
-          <div
-            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3"
-            style={{
-              gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-            }}
-          >
-            {data?.map((schedule: any) => (
-              <ScheduleCard
-                key={schedule.rowId}
-                id={schedule.rowId}
-                title={schedule.title}
-                description={schedule.description}
-                type={schedule.type}
-                interval={schedule.interval}
-                active={schedule.active}
-                setData={setData}
-              />
-            ))}
+  return (
+    <div className="grow p-4 flex flex-col">
+      <h1 className="text-3xl font-bold mb-4">My events</h1>
+      {data && data.length > 0 ? (
+        <div
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3"
+          style={{
+            gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+          }}
+        >
+          {data.map((schedule) => (
+            <ScheduleCard
+              key={schedule.rowId}
+              queryId={queryId}
+              id={schedule.rowId}
+              title={schedule.title}
+              description={schedule.description}
+              type={schedule.type}
+              interval={schedule.interval}
+              active={schedule.active}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="grow content-center self-center bg-gray-100 text-gray-800">
+          <div className="text-2xl text-gray-600">
+            <p>이벤트가 없습니다. 새로운 이벤트를 생성하세요</p>
           </div>
-        ) : (
-          <div className="flex items-center justify-center h-screen bg-gray-100 text-gray-800">
-            <div className="text-2xl text-gray-600">
-              <p>이벤트가 없습니다. 새로운 이벤트를 생성하세요</p>
-            </div>
-          </div>
-        )}
-      </div>
+        </div>
+      )}
       <div>
         <CreateButton
+          queryId={queryId}
           title="New event"
           scheduleType="one_time"
-          setData={setData}
         />
       </div>
     </div>
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,75 +2,53 @@
 
 import ScheduleCard from "@/components/schedule-card";
 import { getScheduleList } from "@/pages/api/schedule";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  const [data, setData] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string>("");
+  const queryId = "main";
+  const { data, isLoading, isError } = useQuery({
+    queryKey: [queryId],
+    queryFn: () => getScheduleList(),
+  });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-
-        const response = await getScheduleList();
-
-        const scheduleList = response;
-
-        if (!scheduleList) {
-          throw new Error("failed to fetch data");
-        }
-        setData(scheduleList);
-      } catch (error: unknown) {
-        if(error instanceof Error) {
-          setError(error.message);
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  if (loading) {
+  if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
-  if (error) {
+  if (isError) {
     return <h1>Error</h1>;
   }
 
   return (
-      <div className="p-4">
-        <h1 className="text-3xl font-bold mb-4">All schedule</h1>
-        {data.length > 0 ? (
-          <div
-            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3"
-            style={{
-              gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-            }}
-          >
-            {data?.map((schedule: any) => (
-              <ScheduleCard
-                key={schedule.rowId}
-                id={schedule.rowId}
-                title={schedule.title}
-                description={schedule.description}
-                type={schedule.type}
-                interval={schedule.interval}
-                active={schedule.active}
-                setData={setData}
-              />
-            ))}
+    <div className="p-4">
+      <h1 className="text-3xl font-bold mb-4">All schedule</h1>
+      {data && data.length > 0 ? (
+        <div
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3"
+          style={{
+            gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+          }}
+        >
+          {data.map((schedule: any) => (
+            <ScheduleCard
+              key={schedule.rowId}
+              queryId={queryId}
+              id={schedule.rowId}
+              title={schedule.title}
+              description={schedule.description}
+              type={schedule.type}
+              interval={schedule.interval}
+              active={schedule.active}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-screen bg-gray-100 text-gray-800">
+          <div className="text-2xl text-gray-600">
+            <p>스케줄이 없습니다</p>
           </div>
-        ) : (
-          <div className="flex items-center justify-center h-screen bg-gray-100 text-gray-800">
-            <div className="text-2xl text-gray-600">
-              <p>스케줄이 없습니다</p>
-            </div>
-          </div>
-        )}
-      </div>
+        </div>
+      )}
+    </div>
   );
 }
